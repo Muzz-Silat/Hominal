f3b63240f416b8f8b7aedb0ee2da54721df65889
Mo Shahid <saadshahidqavi2003@gmail.com>
2023-06-21 17:08:14 +0100

    working new terminal input- beta

--- TypeTest/frontend/static/js/test.js
+++ TypeTest/frontend/static/js/test.js
@@ -1,53 +1,64 @@
-const tag = `C:\\User\\TypeTest>`;
+var tagElement = document.getElementById("tag")
+var tag = tagElement.innerHTML
 
-let input = document.getElementsByClassName("terminal-input")[0];
+var inputElement = document.getElementById("terminalInput")
+let input = document.getElementById("input");
+
 input.focus();
-cursorHandler();
 
 
+// function cursorHandler(){
+//     //ensures the tag remains the same
+//     if(!(input.value.includes(tag)) || input.value[0] != 'C'){
+//         input.value = tag;
+//     }
 
-function cursorHandler(){
-    //ensures the tag remains the same
-    if(!(input.value.includes(tag)) || input.value[0] != 'C'){
-        input.value = tag;
-    }
+//     //keeps the textarea size good
+//     input.style.height = 'auto';
+//     input.style.height =
+//     (input.scrollHeight) + 'px';
 
-    //keeps the textarea size good
-    input.style.height = 'auto';
-    input.style.height =
-    (input.scrollHeight) + 'px';
+//     if(input.selectionStart == input.selectionEnd && input.selectionStart < tag.length){
+//         input.selectionStart = input.selectionEnd = tag.length;
+//     }
+// }
 
-    if(input.selectionStart == input.selectionEnd && input.selectionStart < tag.length){
-        input.selectionStart = input.selectionEnd = tag.length;
-    }
-}
+// input.addEventListener('input', cursorHandler)
+// document.addEventListener('click', cursorHandler)
+// input.addEventListener('keyup', cursorHandler)
 
-input.addEventListener('input', cursorHandler)
-document.addEventListener('click', cursorHandler)
-input.addEventListener('keyup', cursorHandler)
-
-document.getElementById('command')
+document.getElementById('input')
 .addEventListener('keyup', function(event) {
     if (event.code === 'Enter'){
         //event.preventDefault();
         //document.querySelector('form').submit();
 
         //formatting output from the textarea to the true user input (inputValue)
-        let inputValue = input.value;
-        inputValue = inputValue.replace(tag, "")
+        let inputValue = convertToPlain(input.innerHTML);
         console.log("isolated input: " + inputValue)
 
         //handing off input to the input handler
         inputResponse(inputValue)
 
-        let newline = document.createElement("span");
+        let newline = document.createElement("span")
+        newline.style.display = "block"
         console.log(input.value)
-        newline.innerHTML = input.value;
-        input.before(newline)
-        input.value = tag;
+        newline.innerHTML = tag + input.innerHTML;
+        inputElement.before(newline)
+        input.innerHTML = "";
     }
 });
 
+function convertToPlain(html){
+    // Create a new div element
+    var tempDivElement = document.createElement("div");
+
+    // Set the HTML content with the given value
+    tempDivElement.innerHTML = html;
+
+    // Retrieve the text property of the element 
+    return tempDivElement.textContent || tempDivElement.innerText || "";
+}
 
 function inputResponse(inputVal) {   
     fetch(`/commands/${inputVal}`)
@@ -60,7 +71,7 @@ function inputResponse(inputVal) {
             
         }else{
             newline.innerHTML = text;
-            input.before(newline)
+            inputElement.before(newline)
         }
     });
 }
