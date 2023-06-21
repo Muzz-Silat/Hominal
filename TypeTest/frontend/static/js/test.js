6674d0a4eb804e58de570bb8ed46797c3b9c7448
Mo Shahid <saadshahidqavi2003@gmail.com>
2023-06-21 14:54:07 +0100

    working-interface

--- TypeTest/frontend/static/js/test.js
+++ TypeTest/frontend/static/js/test.js
@@ -1,7 +1,7 @@
 const tag = `C:\\User\\TypeTest>`;
 
 let input = document.getElementsByClassName("terminal-input")[0];
-
+console.log(input)
 input.focus();
 cursorHandler();
 
@@ -32,3 +32,23 @@ document.getElementById('command')
         document.querySelector('form').submit();
     }
 });
+
+console.log("cum")
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(response.innerHTML)
+});
+
+
+// xhr = new XMLHttpRequest();
+// var url = "url";
+// xhr.open("POST", url, true);
+// xhr.setRequestHeader("Content-type", "application/json");
+// xhr.onreadystatechange = function () { 
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         var json = JSON.parse(xhr.responseText);
+//         console.log(json.email + ", " + json.name)
+//     }
+// }
+
+// var data = JSON.stringify({"email":"tomb@raider.com","name":"LaraCroft"});
+// xhr.send(data);
--- TypeTest/frontend/views.py
+++ TypeTest/frontend/views.py
@@ -5,17 +5,15 @@ from django.template import loader
 def homepage(request):
     template = loader.get_template('homepage.html')
     if request.method == 'POST':
-        if len(command) > 0:
-            command = request.POST['command']
-            command = command.removeprefix("C:\\User\\TypeTest>")
-            command = command.split() [0]
-            print(command)
-            context = {}
-            if command == 'help':
-                context = {
-                    'response': 'TESTING',
-                }
-                return render(request, 'homepage.html', {'context': context})
+        command = request.POST['command']
+        command = command.removeprefix("C:\\User\\TypeTest>")
+        command = command.split() [0]
+        print(command)
+        context = {}
+        if command == 'help':
+            context = {
+                'response': 'TESTING',
+            }
+            return render(request, 'homepage.html', {'context': context})
     else:
-        print('not a post request')
-    return render(request, 'homepage.html')
+        return render(request, 'homepage.html')
